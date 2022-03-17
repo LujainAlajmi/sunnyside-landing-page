1963ed96a0b59c456a9f0ecbf2b2d18dde3199fd
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2022-03-17 20:06:05 +0300

    mobile settings done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import GalleryConeMobile from "../images/mobile/image-gallery-cone.jpg";
+import GalleryMilkbottlesMobile from "../images/mobile/image-gallery-milkbottles.jpg";
+import GalleryOrangeMobile from "../images/mobile/image-gallery-orange.jpg";
+import GallerySugarCubesMobile from "../images/mobile/image-gallery-sugar-cubes.jpg";
+
+import GalleryConeDesktop from "../images/desktop/image-gallery-cone.jpg";
+import GalleryMilkbottlesDesktop from "../images/desktop/image-gallery-milkbottles.jpg";
+import GalleryOrangeDesktop from "../images/desktop/image-gallery-orange.jpg";
+import GallerySugarCubesDesktop from "../images/desktop/image-gallery-sugarcubes.jpg";
 
 function Gallery() {
-  return <div>Gallery</div>;
+  return (
+    <div>
+      {/* mobile settings */}
+      <div className="xs:flex flex-row sm:hidden">
+        <div className="flex flex-row">
+          <div className="flex flex-col w-1/2">
+            <img src={GalleryMilkbottlesMobile} alt="" />
+            <img src={GalleryConeMobile} alt="" />
+          </div>
+          <div className="flex flex-col w-1/2">
+            <img src={GalleryOrangeMobile} alt="" />
+            <img src={GallerySugarCubesMobile} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Gallery;
