9c66b3582b2bc01ea52874387190fc69c7dfb8d5
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2022-03-17 15:02:24 +0300

    desktop done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,7 +22,7 @@ function Main() {
             </p>
           </div>
 
-          <img src={GlassMobile} alt="egg" className=" object-cover" />
+          <img src={GlassMobile} alt="glass" className=" object-cover" />
           <div className="flex flex-col text-center justify-center  bg-[#fefbf8] py-5">
             <h1 className="px-8 py-5 text-3xl text-VeryDarkDesaturatedBlue">
               Stand out to the right audience
@@ -33,6 +33,42 @@ function Main() {
               extend your brand in digital places.
             </p>
             <p className="text-base py-6 text-VeryDarkDesaturatedBlue uppercase underline  decoration-[5px] decoration-[#faddd2] hover:decoration-SoftRed">
+              <a href="#">Learn more</a>
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* desktop settings */}
+      <div className="sm:flex flex-wrap w-screen xs:hidden">
+        <div className="flex flex-row">
+          <div className="flex flex-col justify-center  bg-[#fefbf8] py-5 text-left">
+            <h1 className="px-32  py-8 text-5xl text-VeryDarkDesaturatedBlue">
+              Transform your brand
+            </h1>
+            <p className="text-lg font-[Barlow] px-32 text-DarkGrayishBlue ">
+              We are a full-service creative agency specializing in helping
+              brands grow fast. Engage your clients through compelling visuals
+              that do most of the marketing for you.
+            </p>
+            <p className="text-base px-36 py-6 text-VeryDarkDesaturatedBlue uppercase underline  decoration-[5px] decoration-[#fdf1b5] hover:decoration-Yellow">
+              <a href="#">Learn more</a>
+            </p>
+          </div>
+          <img src={EggMobile} alt="egg" className=" w-[720px]" />
+        </div>
+
+        <div className="flex flex-row ">
+          <img src={GlassMobile} alt="glass" className="w-[720px]" />
+          <div className="flex flex-col justify-center  bg-[#fefbf8] py-5 text-left">
+            <h1 className="px-32 py-5 text-4xl text-VeryDarkDesaturatedBlue">
+              Stand out to the right audience
+            </h1>
+            <p className="text-lg font-[Barlow] px-32 text-DarkGrayishBlue ">
+              Using a collaborative formula of designers, researchers,
+              photographers, videographers, and copywriters, we’ll build and
+              extend your brand in digital places.
+            </p>
+            <p className="text-base px-36 py-6 text-VeryDarkDesaturatedBlue uppercase underline  decoration-[5px] decoration-[#faddd2] hover:decoration-SoftRed">
               <a href="#">Learn more</a>
             </p>
           </div>
