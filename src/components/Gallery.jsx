b0778ecf31ef1e2d626cd5d1f7b627b64eec2fcb
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2022-03-17 16:27:15 +0300

    desktop done

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -20,6 +20,23 @@ function Gallery() {
           </p>
         </div>
       </div>
+      {/* desktop settings */}
+      <div className="sm:flex flex-row h-screen  xm:hidden">
+        <div className="flex flex-col w-1/2 bg-imageGraphicDesignDesktop bg-cover bg-center bg-no-repeat  text-center justify-center text-DarkDesaturatedCyan pt-52">
+          <h1 className="text-4xl mt-44">Graphic design</h1>
+          <p className="font-[Barlow] text-lg mt-10 px-44">
+            design makes you memorable. We deliver artwork that underscores your
+            brand message and captures potential clients’ attention.
+          </p>
+        </div>
+        <div className="flex flex-col w-1/2 bg-ImagePhotographyDesktop bg-cover bg-center bg-no-repeat  text-center justify-center text-DarkBlue pt-52">
+          <h1 className="text-4xl mt-44">Photography</h1>
+          <p className="font-[Barlow] text-lg mt-10 px-44">
+            Increase your credibility by getting the most stunning, high-quality
+            photos that improve your business image.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
