e280943b844850c25d5306661ca371f5eb0e9699
Lujain <82683327+LujainAlajmi@users.noreply.github.com>
2022-03-17 19:41:19 +0300

    Testimonials done on desktop

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -6,6 +6,7 @@ import Thomas from "../images/image-thomas.jpg";
 function Testimonials() {
   return (
     <div>
+      {/* mobile settings */}
       <div className="xs:flex flex-col w-screen text-center py-10 text-DarkGrayishBlue   bg-[#fefbf8] sm:hidden">
         <h1 className=" uppercase tracking-widest">Client testimonials</h1>
         <div className="flex flex-col items-center py-8">
@@ -40,6 +41,47 @@ function Testimonials() {
           <p className="font-[Barlow]  text-xs text-GrayishBlue">
             Business Owner
           </p>
+        </div>
+      </div>
+      {/* desktop settings */}
+      <div className="sm:flex flex-col w-screen text-center justify-center items-center py-28 text-DarkGrayishBlue   bg-[#fefbf8] xs:hidden">
+        <h1 className=" uppercase tracking-widest text-3xl py-16">
+          Client testimonials
+        </h1>
+        <div className="flex flex-row p-10">
+          <div className="flex flex-col items-center py-8">
+            <img src={Emily} alt="" className="rounded-full w-20" />
+            <p className=" font-[Barlow] px-12 py-7 text-VeryDarkGrayishBlue">
+              We put our trust in Sunnyside and they delivered, making sure our
+              needs were met and deadlines were always hit.
+            </p>
+            <h1 className="text-VeryDarkDesaturatedBlue pb-2">Emily R.</h1>
+            <p className="font-[Barlow]  text-xs text-GrayishBlue">
+              Marketing Director
+            </p>
+          </div>
+          <div className="flex flex-col items-center py-8">
+            <img src={Thomas} alt="" className="rounded-full w-20 " />
+            <p className=" font-[Barlow] px-12 py-7 text-VeryDarkGrayishBlue">
+              Sunnyside’s enthusiasm coupled with their keen interest in our
+              brand’s success made it a satisfying and enjoyable experience.
+            </p>
+            <h1 className="text-VeryDarkDesaturatedBlue pb-2">Thomas S.</h1>
+            <p className="font-[Barlow]  text-xs text-GrayishBlue">
+              Chief Operating Officer
+            </p>
+          </div>
+          <div className="flex flex-col items-center py-8">
+            <img src={Jennie} alt="" className="rounded-full w-20" />
+            <p className=" font-[Barlow] px-12 py-7 text-VeryDarkGrayishBlue">
+              Incredible end result! Our sales increased over 400% when we
+              worked with Sunnyside. Highly recommended!
+            </p>
+            <h1 className="text-VeryDarkDesaturatedBlue pb-2">Jennie F.</h1>
+            <p className="font-[Barlow]  text-xs text-GrayishBlue">
+              Business Owner
+            </p>
+          </div>
         </div>
       </div>
     </div>
